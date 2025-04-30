3b947d04cebe92adcd6bd76c990aaf28eeb932aa
skyzo-free <ayalatifi6@gmail.com>
2025-04-30 16:53:47 +0100

    Complete Navbar and  Reusable Widget Components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,11 @@
 import "./navbar.scss";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined"; 
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlinedIcon from "@mui/icons-material/ChatBubbleOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
  const Navbar = () => {
   return (
@@ -9,7 +15,37 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
           <input type="text" placeholder="Search..."/>
           <SearchOutlinedIcon />
         </div>
-        <div className="items"></div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon"/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon  className="icon"/>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon  className="icon"/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon  className="icon"/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlinedIcon  className="icon"/>
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon  className="icon"/>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg"
+              alt="avatar"
+              className="avatar"
+            />
+
+          </div>
+        </div>
       </div>
     </div>
   )
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -78,7 +78,6 @@ const Sidebar = () => {
         <div className="bottom">
           <div className="colorOption"></div>
           <div className="colorOption"></div>
-          <div className="colorOption"></div>
         </div>
     </div>
   )
