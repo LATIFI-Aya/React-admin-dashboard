b301d0ccf341269ec5fd44669388ab9fe145cd8f
skyzo-free <ayalatifi6@gmail.com>
2025-05-02 19:58:54 +0100

    How to Use React Router Dom Links

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,6 +1,7 @@
 import "./datatable.scss";
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
 
 const Datatable = () => {
 
@@ -8,10 +9,12 @@ const Datatable = () => {
     field: 'action',
     headerName: 'Action',
     width: 200,
-    renderCell: (params) => {
+    renderCell: () => {
       return (
         <div className="cellAction">
+          <Link to="/users/test" style={{textDecoration: "none"}} >
           <div className="viewButton" > View</div>
+          </Link>
           <div className="deleteButton" > Delete</div>
         </div>
       );
@@ -21,6 +24,12 @@ const Datatable = () => {
 
   return (
     <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new"  className="link">
+        Add New
+        </Link>
+      </div>
       <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,29 +11,39 @@ import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
 import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { Link } from "react-router-dom";
 
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <div className="top"><span className="logo"> Doorway Dreams</span></div>
+        <div className="top">
+          <Link to="/" style={{textDecoration: "none"}} >
+          <span className="logo"> Doorway Dreams</span>
+          </Link>
+        </div> 
         <hr />
         <div className="center"> 
             <ul>
                 <p className="title">MAIN</p>
                 <li>
                   <DashboardIcon className="icon"/>
-                  <span>Dashboard</span>
+                     <span>Dashboard</span>
+                  
                 </li>
                 <p className="title">LISTS</p>
+                <Link to="/users" style={{textDecoration: "none"}} >
                 <li>
                   <PersonOutlineIcon className="icon"/>
                   <span>Users</span>
                 </li>
+                </Link>
+                <Link to="/products" style={{textDecoration: "none"}} >
                 <li>
                   <StoreIcon className="icon" />
                   <span>Products</span>
                 </li>
+                </Link>
                 <li>
                   <CreditCardIcon className="icon"/>
                   <span>Orders</span>
