3fa9dad9f1cbeb355ffc665e9f894cd4d7578576
skyzo-free <ayalatifi6@gmail.com>
2025-05-02 19:29:49 +0100

    React Router Dom Links

---
+++ src/formSource.jsx
@@ -0,0 +1,77 @@
+export const userInputs = [
+    {
+        id: 1,
+        label: "Username",
+        type: "text",
+        placeholder: "john_doe",
+    },
+    {
+        id: 2,
+        label: "Name and surname",
+        type: "text",
+        placeholder: "john Doe",
+    },
+    {
+        id: 3,
+        label: "Email",
+        type: "mail",
+        placeholder: "john_doe@gmail.com",
+    },
+    {
+        id: 4,
+        label: "Phone",
+        type: "text",
+        placeholder: "+212 6777777",
+    },
+    {
+        id: 5,
+        label: "Password",
+        type: "password",
+    },
+    {
+        id: 6,
+        label: "Address",
+        type: "text",
+        placeholder: "rue ezziraoui casablanca",
+    },
+    {
+        id: 7,
+        label: "Country",
+        type: "text",
+        placeholder: "morocco",
+    },
+];
+
+export const productInputs = [
+    {
+        id: 1,
+        label: "Title",
+        type: "text",
+        placeholder: "Apple Macbook Pro",
+    },
+    {
+        id: 2,
+        label: "Description",
+        type: "text",
+        placeholder: "Description",
+    },
+    {
+        id: 3,
+        label: "Categories",
+        type: "text",
+        placeholder: "Cumputers",
+    },
+    {
+        id: 4,
+        label: "Price",
+        type: "text",
+        placeholder: "100",
+    },
+    {
+        id: 5,
+        label: "Stock",
+        type: "text",
+        placeholder: "in stock",
+    },
+    
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,55 +2,42 @@ import "./new.scss";
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 //import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+//CloudUploadOutlinedIcon
+import FolderIcon from '@mui/icons-material/Folder';
+import { useState } from "react";
 
-const New = () => {
+
+const New = ({inputs, title}) => {
+
+  const [file, setFile] = useState(null);
+
+  console.log(file)
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Add New User</h1>
+          <h1>{title} </h1>
         </div>
         <div className="bottom">
           <div className="left">
-            <img src="https://icon-library.com/images/no-image-icon-0.jpg" alt="" />
+            <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-images-icon/no-image-icon-0.jpg" } alt="" />
           </div>
           <div className="right">
             <form >
               <div className="formInput">
                 <label htmlFor="file">
-                
+                Image: <FolderIcon />
                 </label>
-                <input type="file" id="file" style={{display: "none"}} />
+                <input type="file" id="file" onChange={e=>setFile(e.target.files[0])} style={{display: "none"}} />
               </div>
-              <div className="formInput">
-                <label >Username</label>
-                <input type="text" placeholder="john_doe"/>
-              </div>
-              <div className="formInput">
-                <label >Name and surname</label>
-                <input type="text" placeholder="John Doe"/>
-              </div>
-              <div className="formInput">
-                <label >Email</label>
-                <input type="email" placeholder="john_doe@gmail.com"/>
-              </div>
-              <div className="formInput">
-                <label >Phone</label>
-                <input type="text" placeholder="+212677888889"/>
-              </div>
-              <div className="formInput">
-                <label >Password</label>
-                <input type="password" />
-              </div>
-              <div className="formInput">
-                <label >Address</label>
-                <input type="text" placeholder="Maarif gomycode rue"/>
-              </div><div className="formInput">
-                <label >Country</label>
-                <input type="password" placeholder="morocco"/>
-              </div>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label htmlFor="">{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              )) }
               <button>Send</button>
             </form>
           </div>
